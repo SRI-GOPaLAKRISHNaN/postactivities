b95776a15219f26d69aed734aa9e7f0ff9ea8239
SRI GOPALAKRISHNAN <gopalsrithar99@gmail.com>
2022-03-16 23:48:30 +0530

    Security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,7 +64,7 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt => opt
+            app.UseCsp(opt => opt
                 .BlockAllMixedContent()
                 .StyleSources(s => s.Self().CustomSources(
                     "https://fonts.googleapis.com",
